67ef27ca0ee5b7fd509dd939c794510186d5e1ef
Dominique Huffman <dominiquebarreca@gmail.com>
2025-10-29 17:13:12 -0400

    snake_game

--- main.py
+++ main.py
@@ -1,46 +1,34 @@
-# Day 19 - Instances, State and Higher Order Functions
-
-# Turtle Race
-
-from turtle import Turtle, Screen
-import random
+from turtle import Screen, Turtle
 
 screen = Screen()
-screen.colormode(255)
+screen.setup(width = 600, height = 600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
 
-is_race_on = False
-screen.setup(width =500, height = 400)
-user_bet = screen.textinput(title = "Make your bet", prompt = "Which turtle will win the race? Enter a color: ")
-colors = ["red", "orange", "yellow", "green", "blue", "purple"]
-x_start = -230
-y_start = -75
-all_turtles = []
+x_start = 0
+segments = []
 
-#Create different instances of the Turtle class that act and behave differently.
-for i in range(6):
-    new_turtle = Turtle(shape ="turtle")
-    new_turtle.speed(0)
-    new_turtle.color(colors[i])
-    new_turtle.penup()
-    new_turtle.goto(x_start, y_start)
-    y_start += 30
-    all_turtles.append(new_turtle)
+for i in range(3):
+    new_segment = Turtle(shape = "square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(x_start, 0)
+    x_start -= 20
+    segments.append(new_segment)
+screen.update()
 
-if user_bet:
-    is_race_on = True
+game_is_on = True
 
-while is_race_on:
-    for turtle in all_turtles:
-        if turtle.xcor() >= 230:
-            is_race_on = False
-            winning_color = turtle.pencolor()
-            if winning_color == user_bet:
-                print(f"The {winning_color} turtle won! You win!")
-            else:
-                print(f"The {winning_color} turtle won, but you bet on {user_bet}. Sorry, you lose.")
-        else:
-            random_distance = random.randint(0,10)
-            turtle.forward(random_distance)
+while game_is_on:
+    for seg in segments:
+        seg.forward(20)
+
+
+
+
+
+
 
 
 
